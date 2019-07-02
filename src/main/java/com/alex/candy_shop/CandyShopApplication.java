a69e7d95d4a52fa24af024f36db6710b9c28a85d
AlexBobryshev <darknessrider1@rambler.ru>
2019-07-03 00:02:18 +0300

    init commit

---
+++ src/main/java/com/alex/candy_shop/CandyShopApplication.java
@@ -0,0 +1,13 @@
+package com.alex.candy_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandyShopApplication.class, args);
+    }
+
+}
