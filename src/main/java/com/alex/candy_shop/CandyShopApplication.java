2935801cb89c2928e146278352e39a3111d5bfcf
AlexBobryshev <darknessrider1@rambler.ru>
2019-12-07 00:11:02 +0300

    "CandyShopApplication class refactored"

--- src/main/java/com/alex/candy_shop/CandyShopApplication.java
+++ src/main/java/com/alex/candy_shop/CandyShopApplication.java
@@ -2,7 +2,6 @@ package com.alex.candy_shop;
 
 import com.alex.candy_shop.entities.Product;
 import com.alex.candy_shop.entities.User;
-import com.alex.candy_shop.repos.OrderRepo;
 import com.alex.candy_shop.repos.ProductRepo;
 import com.alex.candy_shop.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,43 +11,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CandyShopApplication {
     @Autowired
     PasswordEncoder encoder;
 
     @Bean
-    public CommandLineRunner dataLoader(UserRepo userRepo, ProductRepo productRepo, OrderRepo orderRepo) {
+    public CommandLineRunner dataLoader(UserRepo userRepo, ProductRepo productRepo) {
         return args -> {
-            /*
-            User user = new User();
-            user.setUsername("user1");
-            user.setPassword(encoder.encode("pass"));
-            user.setMoneyBalance(200);
-            userRepo.save(user);
-            */
+            // adds a new User if the DB still doesn't contain at least one
+            if (((List) userRepo.findAll()).isEmpty()) {
+                User user = new User();
+                user.setUsername("user1");
+                user.setPassword(encoder.encode("pass"));
+                user.setMoneyBalance(200);
+                userRepo.save(user);
+            }
 
-            /*
-            Product sneakers = new Product("sneakers", 1.1, 10);
-            Product twins = new Product("twins", 0.75, 0);
-            Product kidcut = new Product("kidcut", 0.8, 5);
-            Product bouncy = new Product("bouncy", 0.9, 59);
-            Product earth = new Product("earth", 0.95, 7);
-            */
+            // adds Product items if the DB still doesn't contain at least one
+            if (((List) productRepo.findAll()).isEmpty()) {
+                Product sneakers = new Product("sneakers", 1.1, 10);
+                Product twins = new Product("twins", 0.75, 0);
+                Product kidcut = new Product("kidcut", 0.8, 5);
+                Product bouncy = new Product("bouncy", 0.9, 59);
+                Product earth = new Product("earth", 0.95, 7);
 
-            //sneakers.setInStock(1000);
-            //twins.setInStock(1000);
-            //kidcut.setInStock(1000);
-            //bouncy.setInStock(1000);
-            //earth.setInStock(1000);
-
-            /*
-            productRepo.save(sneakers);
-            productRepo.save(twins);
-            productRepo.save(kidcut);
-            productRepo.save(bouncy);
-            productRepo.save(earth);
-            */
+                productRepo.save(sneakers);
+                productRepo.save(twins);
+                productRepo.save(kidcut);
+                productRepo.save(bouncy);
+                productRepo.save(earth);
+            }
         };
     }
 
